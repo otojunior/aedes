554a7fdd9e5af64b58e591d261f606940a710275
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2025-09-28 10:46:35 -0300

    Início de implementação com algumas operações

---
+++ src/main/java/com/github/otojunior/aedes/Lista.java
@@ -0,0 +1,155 @@
+/**
+ * 
+ */
+package com.github.otojunior.aedes;
+
+/**
+ * Estrutura de dados Lista.
+ * @author Oto Soares Coelho Junior (otojunior@gmail.com)
+ * @since 28/09/2025
+ */
+public class Lista {
+    /**
+     * Estrurura de dados No
+     * @author Oto Soares Coelho Junior (otojunior@gmail.com)
+     * @since 28/09/2025
+     */
+    private class No {
+        private int valor;
+        private No anterior;
+        private No proximo;
+
+        private No(int valor) {
+            this.valor = valor;
+        }
+    }
+
+    private int tamanho;
+    private No primeiro;
+    private No ultimo;
+
+    /**
+     * Insere um elemento no fim da lista.
+     * @param valor Valor a ser inserido.
+     */
+    public void inserir(int valor) {
+        No novo = new No(valor);
+
+        // Caso 1 (if): Lista vazia: inserção do primeiro elemento.
+        if (this.primeiro == null && this.ultimo == null) {
+            this.primeiro = novo;
+            this.ultimo = novo;
+        }
+
+        // Caso 2 (else): Lista não vazia: inserção dos demais elementos.
+        else {
+            this.ultimo.proximo = novo;
+            novo.anterior = this.ultimo;
+            this.ultimo = this.ultimo.proximo;
+        }
+        this.tamanho++;
+    }
+
+    /**
+     * Insere elementos em posição específica da lista.
+     * @param posicao Posição específica na qual o elemento será inserido.
+     * @param valor Valor do elemento.
+     */
+    public void inserir(int posicao, int valor) {
+        No novo = new No(valor);
+
+        // Caso 1: Lista vazia.
+        if (posicao == 0 && this.primeiro == null && this.ultimo == null) {
+            this.primeiro = novo;
+            this.ultimo = novo;
+        } else {
+            No atual = this.obter(posicao);
+            
+            // Caso 2: Inserir no inicio.
+            if (atual == this.primeiro) {
+                novo.proximo = atual;
+                atual.anterior = novo;
+                this.primeiro = novo;
+            }
+            
+            // Caso 3: Inserir no meio ou no fim.
+            else {
+                No esquerda = atual.anterior;
+                No direita = atual;
+                novo.anterior = esquerda;
+                esquerda.proximo = novo;
+                novo.proximo = direita;
+                direita.anterior = novo;
+            }
+        }
+        tamanho++;
+    }
+
+    /**
+     * Retorna o tamanho da lista.
+     * @return Tamanho da lista (número de elementos na lista).
+     */
+    public int tamanho() {
+        return this.tamanho;
+    }
+
+    /**
+     * Imprime a lista de frente para trás caso contenha elementos.
+     * @return Elementos da lista ou string (Lista Vazia)
+     */
+    @Override
+    public String toString() {
+        if (this.tamanho > 0) {
+            StringBuilder str = new StringBuilder();
+            No atual = this.primeiro;
+            for (int i = 0; i < this.tamanho; i++) {
+                str.append(atual.valor).append(' ');
+                atual = atual.proximo;
+            }
+            str.deleteCharAt(str.length()-1);
+            return str.toString();
+        } else {
+            return "(Lista Vazia)";
+        }
+    }
+    
+    /**
+     * Obtém o No referente à posição específica na lista.
+     * @param posicao Posição requerida.
+     * @return No requerido
+     */
+    private No obter(int posicao) {
+        if (posicao >= 0 && posicao < tamanho) {
+            /*
+             * Começa a pesquisar de frente para trás caso a posição
+             * requerida esteja mais próximo do início da lista. 
+             */
+            if (posicao < this.tamanho / 2) {
+                No atual = this.primeiro;
+                for (int i = 0; i != posicao && i < this.tamanho; i++)
+                    atual = atual.proximo;
+                return atual;
+            }
+            /*
+             * Começa a pesquisar de trás para frente caso a posição
+             * requerida esteja mais próximo do fim da lista. 
+             */
+             else {
+                 No atual = this.ultimo;
+                 for (int i = this.tamanho-1 ; i != posicao && i >= 0; i--)
+                     atual = atual.anterior;
+                 return atual;
+            }
+        } else {
+            if (this.tamanho == 0) {
+                throw new IndexOutOfBoundsException("Lista vazia.");
+            } else {
+                throw new IndexOutOfBoundsException(
+                    ("Erro ao acessar a posição %d. " +
+                    "Só é permitido posições entre 0 e %d.")
+                    .formatted(posicao, this.tamanho-1));
+                
+            }
+        }
+    }
+}
---
+++ src/test/java/com/github/otojunior/aedes/ListaTest.java
@@ -0,0 +1,144 @@
+/**
+ * 
+ */
+package com.github.otojunior.aedes;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+/**
+ * Teste de unidade da Lista
+ * @author Oto Soares Coelho Junior (otojunior@gmail.com)
+ * @since 28/09/2025
+ */
+class ListaTest {
+    /**
+     * Test method for {@link com.github.otojunior.aedes.Lista#inserir(int)}.
+     */
+    @Test
+    @DisplayName("Inserção em Lista vazia")
+    void testInserirListaVazia() {
+        Lista lista = new Lista();
+        lista.inserir(1);
+        assertEquals("1", lista.toString());
+    }
+
+    /**
+     * Test method for {@link com.github.otojunior.aedes.Lista#inserir(int)}.
+     */
+    @Test
+    @DisplayName("Inserção em Lista já com elementos.")
+    void testInserirListaComElementos() {
+        Lista lista = new Lista();
+        lista.inserir(1);
+        lista.inserir(2);
+        assertEquals("1 2", lista.toString());
+    }
+    
+    /**
+     * Test method for {@link com.github.otojunior.aedes.Lista#inserir(int)}.
+     */
+    @Test
+    @DisplayName("Inserção em posição 0 em lista vazia")
+    void testInserirListaPosicao0SemElementos() {
+        Lista lista = new Lista();
+        lista.inserir(0, 1);
+        assertEquals("1", lista.toString());
+    }
+    
+    /**
+     * Test method for {@link com.github.otojunior.aedes.Lista#inserir(int)}.
+     */
+    @Test
+    @DisplayName("Inserção em posição 0 em lista com 1 elemento")
+    void testInserirListaPosicao0Com1Elemento() {
+        Lista lista = new Lista();
+        lista.inserir(2);
+        lista.inserir(0, 1);
+        assertEquals("1 2", lista.toString());
+    }
+    
+    /**
+     * Test method for {@link com.github.otojunior.aedes.Lista#inserir(int)}.
+     */
+    @Test
+    @DisplayName("Inserção em posição 0 em lista com 2 elementos")
+    void testInserirListaPosicao0Com2Elementos() {
+        Lista lista = new Lista();
+        lista.inserir(2);
+        lista.inserir(3);
+        lista.inserir(0, 1);
+        assertEquals("1 2 3", lista.toString());
+    }
+    
+    /**
+     * Test method for {@link com.github.otojunior.aedes.Lista#inserir(int)}.
+     */
+    @Test
+    @DisplayName("Inserção em posição 1 em lista vazia")
+    void testInserirListaPosicao1SemElementos() {
+        Exception ex = assertThrows(IndexOutOfBoundsException.class, () -> {
+            Lista lista = new Lista();
+            lista.inserir(1, 1);
+            assertEquals("1", lista.toString());
+        });
+        assertEquals("Lista vazia.", ex.getMessage());
+    }
+
+    /**
+     * Test method for {@link com.github.otojunior.aedes.Lista#inserir(int)}.
+     */
+    @Test
+    @DisplayName("Inserção em posição 1 em lista com 1 elemento")
+    void testInserirListaPosicao1Com1Elemento() {
+        Exception ex = assertThrows(IndexOutOfBoundsException.class, () -> {
+            Lista lista = new Lista();
+            lista.inserir(2);
+            lista.inserir(1, 1);
+            assertEquals("1", lista.toString());
+        });
+        assertEquals(
+            "Erro ao acessar a posição 1. "
+            + "Só é permitido posições entre 0 e 0.",
+            ex.getMessage());
+    }
+    
+    /**
+     * Test method for {@link com.github.otojunior.aedes.Lista#inserir(int)}.
+     */
+    @Test
+    @DisplayName("Inserção em posição 1 em lista com 2 elementos")
+    void testInserirListaPosicao1Com2Elementos() {
+        Lista lista = new Lista();
+        lista.inserir(1);
+        lista.inserir(3);
+        lista.inserir(1, 2);
+        assertEquals("1 2 3", lista.toString());
+    }
+
+    /**
+     * Test method for {@link com.github.otojunior.aedes.Lista#tamanho()}.
+     */
+    @Test
+    @DisplayName("Tamanho (quantidade de elementos) da Lista")
+    void testTamanho() {
+        Lista lista = new Lista();
+        lista.inserir(1);
+        lista.inserir(2);
+        lista.inserir(3);
+        assertEquals(3, lista.tamanho());
+    }
+
+    /**
+     * Test method for {@link com.github.otojunior.aedes.Lista#toString()}.
+     */
+    @Test
+    @DisplayName("Impressão da Lista")
+    void testToString() {
+        Lista lista = new Lista();
+        lista.inserir(11);
+        lista.inserir(22);
+        lista.inserir(33);
+        assertEquals("11 22 33", lista.toString());
+    }
+}
